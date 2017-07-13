dce4e442cc36a00ecadc4443ffa4e1472ed4289f
Trey <treyvis@gmail.com>
2017-07-13 10:56:04 -0600

    parsing times and sending JSON

--- index.js
+++ index.js
@@ -4,16 +4,35 @@ const app = express();
 
 
 app.get('/', (req, res) => {
-	console.log('server pinged');
-	res.send('you found me');
+	console.log('/ Pinged');
+	res.send('<h1>Timestamp Microservice</h1>');
 });
 
 app.get('/:date', (req, res) => {
-	res.send(req.params.date);
-	if (+req.params.date) {
-		res.send(moment(req.params.date, 'X'));
+	console.log('/:date Pinged');
+	const dateString = req.params.date;
+	const reg = /^\d+$/;
+	if (reg.test(dateString)) {
+		res.json({
+			unix: moment(dateString, 'X').format('X'),
+			natural: moment(dateString, 'X').format('MMMM DD, YYYY')
+		});
+	} else {
+		const momentDate = moment(dateString,['MMMM DD, YYYY', 'DD MMMM YYYY', 'MM-DD-YYYY', 'YYYY-MM-DD']);
+		//res.send(momentDate.isValid());
+		if(momentDate.isValid()) {
+			res.json({
+				unix: momentDate.format('X'),
+				natural: momentDate.format('MMMM DD, YYYY')
+			});
+		} else {
+			res.json({
+				unix: null,
+				natural: null
+			});
+		}
 	}
 });
 
 app.listen(4040);
-console.log('pulling express');
+console.log('Listening on 4040');
--- momenttest.js
+++ momenttest.js
@@ -1,5 +1,6 @@
 const moment = require('moment');
 
+console.log(moment('12 Jun 1989', 'MMMM DD YYYY').isValid());
 console.log(moment('Jun 12, 1989', 'MMMM DD, YYYY'));
 console.log(moment('123784789', 'X'));
 console.log(moment('June 12, 1989', 'X'));
