2df50ef6a13cc76fb946be829d4b546705e03f96
Trey <treyvis@gmail.com>
2017-07-13 02:55:04 -0600

    testing moment'

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const moment = require('moment');
 const app = express();
+
 
 app.get('/', (req, res) => {
 	console.log('server pinged');
 	res.send('you found me');
+});
+
+app.get('/:date', (req, res) => {
+	res.send(req.params.date);
+	if (+req.params.date) {
+		res.send(moment(req.params.date, 'X'));
+	}
 });
 
 app.listen(4040);
---
+++ momenttest.js
@@ -0,0 +1,7 @@
+const moment = require('moment');
+
+console.log(moment('Jun 12, 1989', 'MMMM DD, YYYY'));
+console.log(moment('123784789', 'X'));
+console.log(moment('June 12, 1989', 'X'));
+console.log(+"June");
+console.log(+'43124312');
