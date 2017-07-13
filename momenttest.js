bca2b2541fc97ba7b2e7e8abe210642b9f692b57
Trey <treyvis@gmail.com>
2017-07-13 11:17:10 -0600

    Deleting moment test script

--- momenttest.js
+++
@@ -1,8 +0,0 @@
-const moment = require('moment');
-
-console.log(moment('12 Jun 1989', 'MMMM DD YYYY').isValid());
-console.log(moment('Jun 12, 1989', 'MMMM DD, YYYY'));
-console.log(moment('123784789', 'X'));
-console.log(moment('June 12, 1989', 'X'));
-console.log(+"June");
-console.log(+'43124312');
